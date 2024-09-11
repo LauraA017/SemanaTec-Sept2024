a5bd454969366ace8d145e10f498eb4bd6f3a0bb
Laura Cintora <139409765+LauraA017@users.noreply.github.com>
2024-09-10 18:52:15 -0600

    Update promedio.py

--- promedio.py
+++ promedio.py
@@ -1,3 +1,7 @@
-def promedio(num1, num2, num3):
-  return (num1 + num2 + num3)/3
-  
+def average(numbers):
+    sum = 0
+    for num in numbers:
+        sum += num
+    return sum/len(numbers)
+
+print(f"El promedio de [1,2,3] es:"(average [1,2,3]))
